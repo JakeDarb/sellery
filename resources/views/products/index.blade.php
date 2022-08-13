1b6df73c4df24b5cef8d39e6a908fb7f143ca560
Stijn Bouckaert <stijnbouckaert@Stijns-MacBook-Pro.local>
2022-08-13 18:28:23 +0200

    setup[components]: alert

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts/app')
 
 @section('content')
-<h1>Products</h1>
-@foreach($products as $product)
-    <div>
-        <a href="/products/{{ $product->id }}">{{ $product->name }}</a>
-    </div>
-@endforeach
+    @component('components/alert')  
+        @slot('type') success @endslot
+        All products have been loaded
+    @endcomponent
+    <h1>Products</h1>
+    @foreach($products as $product)
+        <div>
+            <a href="/products/{{ $product->id }}">{{ $product->name }}</a>
+        </div>
+    @endforeach
